b6a5daa6a061f551108fcea95012a53e4c828e0d
Dima <stepanovdmitriy94@mail.ru>
2022-04-18 16:38:00 +0300

    Task 1

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Max (int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2>result) result = arg2;
+    if (arg3>result) result = arg3;
+    return result;
+}
+
+int a1 = 12, a2 =154, a3 = 25,
+    a4 = 9, a5 = 34, a6 = 17,
+    a7 = 3, a8 = 99, a9 = 34;
+
+int max = Max(Max(a1,a2,a3),Max(a4,a5,a6),Max(a7,a8,a9));
+
+Console.WriteLine (max);
